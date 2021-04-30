370083af4140b561b37880c652a06f600b3f52f9
Adam10108 <adam_atlas1998@hotmail.com>
2021-05-01 00:25:21 +0700

    Helper styles

--- src/containers/CovidTimeline.tsx
+++ src/containers/CovidTimeline.tsx
@@ -1,10 +1,29 @@
-import { Typography } from "@material-ui/core"
+import { Box, Container, Theme, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import { useStyles } from "../utils/styles"
+
+const customStyles = makeStyles((theme: Theme) => ({
+  titleBox: {
+    [theme.breakpoints.down("xs")]: {
+      textAlign: "center",
+    },
+  },
+}))
 
 const CovidTimeline = (): JSX.Element => {
+  const classes = useStyles()
+  const customClasses = customStyles()
+
   return (
-    <div>
-      <Typography variant="h6">Covid Timeline Infor Graphics</Typography>
-    </div>
+    <Container>
+      <div className={classes.spacerMd}></div>
+
+      <Box className={customClasses.titleBox} width='100%'>
+        <Typography className={classes.textAmber} variant='h1'>
+          COVID Timeline Generator
+        </Typography>
+      </Box>
+    </Container>
   )
 }
 
--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,3 +1,6 @@
+import { Theme } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+
 export const colors = {
   midnightBlue: "#012d5e",
   amber: "#ffc107",
@@ -6,3 +9,18 @@ export const colors = {
   forestGreen: "#27a740",
   astronaut: "#234973",
 }
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  spacerXs: {
+    height: theme.spacing(2),
+  },
+  spacer: {
+    height: theme.spacing(3),
+  },
+  spacerMd: {
+    height: theme.spacing(4),
+  },
+  textAmber: {
+    color: colors.amber,
+  },
+}))
